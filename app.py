54e5c2ae185c14cfa9111d7ca9f67681cef35685
SHEEEEEEEEEEEEEEEEN11 <116128798+SHEEEEEEEEEEEEEEEEN11@users.noreply.github.com>
2024-12-13 03:16:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -605,5 +605,5 @@ def get_traffic_status(congestion):
         return "Congested"
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))# Default to 5000 if PORT is not set
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT",5000))# Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0",port=port)
